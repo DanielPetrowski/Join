764c082ca6fb3c8603f444575b1ea081e6140393
Rohric <emilmarsal@icloud.com>
2025-12-15 16:39:33 +0100

    fix colors

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Login } from "./login/login";
 import { Navbar } from './shared/navbar/navbar';
 import { Header } from './shared/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login,Navbar,Header],
+  imports: [RouterOutlet,Navbar,Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
