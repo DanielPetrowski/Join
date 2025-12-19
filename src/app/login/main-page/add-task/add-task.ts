8b716faf0c126c3020f37ecd41038d4e4511b6d0
DanielPetrowski <petrowskidaniel@web.de>
2025-12-19 13:08:19 +0100

    add basic label

--- src/app/login/main-page/add-task/add-task.ts
+++ src/app/login/main-page/add-task/add-task.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 
 
 @Component({
   selector: 'app-add-task',
   standalone:true,
-  imports: [],
+  imports: [MatFormFieldModule, MatSelectModule, FormsModule, ReactiveFormsModule],
   templateUrl: './add-task.html',
   styleUrl: './add-task.scss',
 })
@@ -19,6 +22,12 @@ export class AddTask {
       this.priority = p;
     }
   }
+
+  toppings = new FormControl('');
+  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
+
+  
+
 }
 
 
