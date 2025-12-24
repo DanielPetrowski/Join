f22e491ce60310de1df37432cc8fff7c751e63f2
Felix R <nikojin@gmx.net>
2025-12-24 14:36:52 +0100

    fix task-preview comment

--- src/app/login/main-page/board/task-preview/task-preview.ts
+++ src/app/login/main-page/board/task-preview/task-preview.ts
@@ -2,36 +2,6 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BoardTask } from "../../../../interfaces/task-board.interface";
 
-// @Component({
-//   selector: 'app-task-preview',
-//   imports: [
-//     CommonModule,
-//   ],
-//   templateUrl: './task-preview.html',
-//   styleUrl: './task-preview.scss',
-//   standalone: true,
-// })
-
-// export class TaskPreview {
-
-//   taskreceived: any;
-
-//   @Input() task?: object;
-
-//   constructor() {
-//     this.taskreceived = this.task;
-//     console.log(this.taskreceived);
-//   }
-
-//   getSubtaskProgress(task: any): number {
-//         if (task.subtasksTotal === 0) {
-//             return 0;
-//         }
-
-//         return (task.subtasksDone / task.subtasksTotal) * 100;
-//     }
-// }
-
 @Component({
   selector: 'app-task-preview',
   standalone: true,
