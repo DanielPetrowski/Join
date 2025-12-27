6d0669a6b35df98522e3774ee52ed97baa872da6
Felix R <nikojin@gmx.net>
2025-12-27 13:14:13 +0100

    feat add task-preview style + firebase logic

--- src/app/login/main-page/board/task-preview/task-preview.ts
+++ src/app/login/main-page/board/task-preview/task-preview.ts
@@ -1,13 +1,28 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BoardTask } from "../../../../interfaces/task-board.interface";
+import { TaskType } from '../../../../types/task-type';
 
 @Component({
   selector: 'app-task-preview',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './task-preview.html',
+  styleUrls: ['./task-preview.scss'],
 })
 export class TaskPreview {
   @Input({ required: true }) task!: BoardTask;
+
+  readonly TaskType = TaskType;
+
+  get taskTypeSvg(): string {
+    switch (this.task.type) {
+      case TaskType.UserStory:
+        return 'img/task_type_user_story.svg';
+      case TaskType.TechnicalTask:
+        return 'img/task_type_technical_task.svg';
+      default:
+        return '';
+    }
+  }
 }
